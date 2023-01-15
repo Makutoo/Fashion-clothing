fc25175a4e314dc33a90f1ad00ce3dde603920aa
Ziheng Zhu <ziheng.zhu@digifi.com>
2023-01-15 12:52:55 -0500

    add filter function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -5,13 +5,16 @@ import './App.css';
 
 class App extends Component {
   constructor() {
+    //console.log('constructor')
     super()
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
   componentDidMount() { // first time component render
+    //console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState(
@@ -25,17 +28,31 @@ class App extends Component {
   }
 
   render() {
+    //console.log('render')
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    });
     return (
       <div className='App'>
-        {
-          this.state.monsters.map((monster) => {
-            return (
-              <div key={monster.id}>
-                <h1 >{monster.name}</h1>
-              </div>
-            )
-          })
-        }
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            console.log(event.target.value);
+            const newSearchField = event.target.value.toLocaleLowerCase()
+            this.setState(() => {
+              return { searchField :  newSearchField}
+            })
+          }}
+        ></input>
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1 >{monster.name}</h1>
+            </div>
+          )
+        })}
       </div>
     )
   }
