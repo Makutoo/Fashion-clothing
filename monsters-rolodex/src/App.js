9aefd750904a63cc9d48d1165a19fcd67f2b4fa6
Ziheng Zhu <ziheng.zhu@digifi.com>
2023-01-17 00:46:32 -0500

    add styles.css and more component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -47,10 +47,11 @@ class App extends Component {
     });
     return (
       <div className='App'>
+        <h1 className='app-title'>Monster Rolodex</h1>
         <SearchBox 
+          className='monsters-search-box'
           onChangeHandler={onSearchChange} 
           placeholder='search monsters'
-          className='search-box'
         />
         <CardList monsters={filteredMonsters}/>
       </div>
--- monsters-rolodex/src/components/card-list/card-list-component.jsx
+++ monsters-rolodex/src/components/card-list/card-list-component.jsx
@@ -1,15 +1,19 @@
 import { Component } from "react";
+import CardContainer from "../card-container/card-container-component";
 
+import './card-list.styles.css'
 class CardList extends Component {
     render() {
         // when return a component make sure there is only top level component to return
-        console.log(this.props)
+        // console.log(this.props)
         const { monsters } = this.props
         return (
-            <div>
-                {monsters.map(monster => (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
+            <div className="card-list">
+                {monsters.map((monster) => {
+                    return (
+                        <CardContainer monster={monster} key={monster.id}/>
+                    )
+                })}
             </div>
         )
     }
